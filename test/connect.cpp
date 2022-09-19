4e4f8b0eb4bf3954c21a15525ee23d955f06d354
StoneHappy <18017158729@163.com>
2022-09-19 14:25:50 +0800

    🧪 test: connect

--- test/connect.cpp
+++ test/connect.cpp
@@ -3,22 +3,23 @@
 #include <gtest/gtest.h>
 #include <netdb.h>
 #include <sys/socket.h>
-TEST(networkprogramming, bind)
+TEST(networkprogramming, connect)
 {
     struct addrinfo hints, *res;
+    // 首先，用 getaddrinfo() 載入 address structs：
+
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
 
-    int status = getaddrinfo(NULL, "3490", &hints, &res);
+    int status = getaddrinfo("www.example.com", "http", &hints, &res);
     if (status !=0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         ASSERT_EQ(status, 0);
 	}
-    int s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    ASSERT_NE(s, -1);
+    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    ASSERT_NE(sockfd, -1);
 
-    status = bind(s, res->ai_addr, res->ai_addrlen);
+    status = connect(sockfd, res->ai_addr, res->ai_addrlen);
     ASSERT_NE(status, -1);
 }
